77d95f6362b281b582b488f8de77a0a9c9e37ac2
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2022-12-27 21:40:56 +0600

    validation added to the auth form

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,9 +1,46 @@
 import React from "react";
+import useInput from "../../hooks/use-input";
 
 import styles from "./LoginForm.module.css";
 import inputStyles from "./input.module.css";
 
+const isNotEmpty = (value: string): boolean => value.trim() !== "";
+const isEmail = (value: string): boolean => {
+  let isValid = true;
+  if (isNotEmpty(value) && value.includes("@") && value.includes(".")) {
+    let values = value.split("@");
+    if (values.length === 2) {
+      if (isNotEmpty(values[0]) && isNotEmpty(values[1])) {
+        values = value.split(".");
+        if (values.length >= 2 && isNotEmpty(values[values.length - 1])) {
+          isValid = true;
+        }
+      }
+    }
+  }
+  return isValid;
+};
+const isPassword = (value: string): boolean => value.trim().length >= 6;
+
 export default function LoginForm() {
+  const {
+    value: email,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
+
+  const {
+    value: password,
+    isValid: passwordIsValid,
+    hasError: passwordHasError,
+    valueChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPassword,
+  } = useInput(isPassword);
+
   return (
     <form className={styles.form}>
       <input
@@ -11,12 +48,16 @@ export default function LoginForm() {
         className={inputStyles.input}
         placeholder="Your Email"
         id="email"
+        onChange={emailChangeHandler}
+        onBlur={emailBlurHandler}
       />
       <input
         type="password"
         className={inputStyles.input}
         placeholder="Your Password"
         id="password"
+        onChange={passwordChangeHandler}
+        onBlur={passwordBlurHandler}
       />
       <button type="submit" className={inputStyles.submit}>
         Sign in
--- components/auth/SignupForm.tsx
+++ components/auth/SignupForm.tsx
@@ -1,9 +1,70 @@
 import React from "react";
+import useInput from "../../hooks/use-input";
 
 import styles from "./SignupFrom.module.css";
 import inputStyles from "./input.module.css";
 
+const isNotEmpty = (value: string): boolean => value.trim() !== "";
+const isEmail = (value: string): boolean => {
+  let isValid = true;
+  if (isNotEmpty(value) && value.includes("@") && value.includes(".")) {
+    let values = value.split("@");
+    if (values.length === 2) {
+      if (isNotEmpty(values[0]) && isNotEmpty(values[1])) {
+        values = value.split(".");
+        if (values.length >= 2 && isNotEmpty(values[values.length - 1])) {
+          isValid = true;
+        }
+      }
+    }
+  }
+  return isValid;
+};
+const isPassword = (value: string): boolean => value.trim().length >= 6;
+
 export default function SignupForm() {
+  const {
+    value: firstName,
+    isValid: firstNameIsValid,
+    hasError: firstNameHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = useInput(isNotEmpty);
+  const {
+    value: lastName,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+  } = useInput(isNotEmpty);
+  const {
+    value: email,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
+
+  const {
+    value: password,
+    isValid: passwordIsValid,
+    hasError: passwordHasError,
+    valueChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPassword,
+  } = useInput(isPassword);
+  const {
+    value: confirmPassword,
+    isValid: confirmPasswordIsValid,
+    hasError: confirmPasswordHasError,
+    valueChangeHandler: confirmPasswordChangeHandler,
+    inputBlurHandler: confirmPasswordBlurHandler,
+    reset: resetcConfirmPassword,
+  } = useInput((value) => value === password);
+
   return (
     <form className={styles.form}>
       <input
@@ -11,31 +72,40 @@ export default function SignupForm() {
         id="first-name"
         placeholder="First Name"
         className={inputStyles.input}
+        onChange={firstNameChangeHandler}
+        onBlur={firstNameBlurHandler}
       />
       <input
         type="text"
         id="last-name"
         placeholder="Last Name"
         className={inputStyles.input}
+        onChange={lastNameChangeHandler}
+        onBlur={lastNameBlurHandler}
       />
       <input
         type="email"
         id="email"
         placeholder="Your Email"
         className={inputStyles.input}
+        onChange={emailChangeHandler}
+        onBlur={emailBlurHandler}
       />
       <input
         type="password"
         id="password"
         placeholder="Your Password"
         className={inputStyles.input}
+        onChange={passwordChangeHandler}
+        onBlur={passwordBlurHandler}
       />
-      {/* <label htmlFor="confirm-password">Confirm Password</label> */}
       <input
         type="confirm-password"
         id="confirm-password"
         placeholder="Confirm Password"
         className={inputStyles.input}
+        onChange={confirmPasswordChangeHandler}
+        onBlur={confirmPasswordBlurHandler}
       />
       <button type="submit" className={inputStyles.submit}>
         Sign up
