0b5a636a55a4aa889af52814beee98d14f774f37
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2023-01-14 18:38:08 +0600

    jwt token updated

--- middleware.ts
+++ middleware.ts
@@ -21,15 +21,15 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  // if (!request.cookies.has("jwtoken")) {
-  //   try {
-  //     const { data } = await axios.put("/auth/refresh");
-  //     Cookies.set("jwtoken", data.accessToken);
-  //     console.log(data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+  if (!request.cookies.has("jwtoken")) {
+    try {
+      const { data } = await axios.put("/auth/refresh");
+      Cookies.set("jwtoken", data.accessToken);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   if (
     !request.cookies.has("jwtoken") &&
     !request.url.endsWith("/login") &&
